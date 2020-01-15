14671cbf3d96a61539af7a9b404e0a1b3e347074
Nate Jensen <nathan.jensen.58@gmail.com>
2020-01-14 17:54:53 -0700

    bootcamp-primer-page added

---
+++ src/components/bootcamp-primer/Course.js
@@ -0,0 +1,89 @@
+import React from "react"
+import styled from "styled-components"
+import { black, gray, orange } from "@vschool/lotus"
+
+const CourseContainer = styled.div`
+    margin: 24px;
+
+    @media (min-width: 840px) {
+        margin-top: 8px;
+        margin-left: 48px;
+    }
+`
+
+const H3 = styled.h3`
+    color: ${black};
+    font-size: 20px;
+    font-weight: 900;
+    line-height: 26px;
+`
+
+const P = styled.p`
+    color: ${gray.darker};
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    height: 80px;
+    margin-top: 16px;
+
+    @media (min-width: 960px) {
+        height: 80px;
+    }
+`
+
+const H4 = styled.h4`
+    color: ${orange.dark};
+    font-size: 16px;
+    font-family: "aktiv-grotesk-extended";
+    font-weight: 800;
+    line-height: 24px;
+    margin-top: 16px;
+`
+
+const Button = styled.a`
+    height: 42px;
+    width: 138px;
+    border: 2px solid ${black};
+    background-color: #fbf9f7;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 4px 4px 0 0 rgba(0, 0, 0, 0.25);
+    color: ${black};
+    font-family: "aktiv-grotesk-extended";
+    font-size: 14px;
+    font-weight: bold;
+    letter-spacing: 0.43px;
+    line-height: 14px;
+    text-align: center;
+    text-decoration: none;
+    cursor: pointer;
+    margin-top: 24px;
+
+    :hover {
+      transform: translate(2px, 2px);
+      box-shadow: 2px 2px 0 0 rgba(100,100,100, 0.5);
+    }
+
+    :active {
+      transform: translate(4px, 4px);
+      box-shadow: inset 1px 1px #eee9;
+      border-top: 1px solid #eee9;
+      border-left: 1px solid #eee9;
+    }
+`
+
+function Course({ course, course_info, course_date, course_link }) {
+    return (
+        <CourseContainer>
+            <H3>{ course.text }</H3>
+            <P>{ course_info.text }</P>
+            <H4>{ course_date.text }</H4>
+            <Button href={ course_link.url } target="_blank">
+                Learn More
+            </Button>
+        </CourseContainer>
+    )
+}
+
+export default Course
---
+++ src/components/bootcamp-primer/Testimonial.js
@@ -0,0 +1,204 @@
+import React from "react"
+import styled from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
+import { gray, orange, blue } from "@vschool/lotus"
+
+const Container = styled.div`
+    width: 100%;
+`
+
+const WhiteBlock = styled.div`
+    background-color: ${gray.lighter};
+    height: 136px;
+
+    @media (min-width: 960px) {
+        height: 102px;
+    }
+`
+
+const OrangeBlock = styled.div`
+    background-color: ${orange.light};
+    padding: 16px;
+    width: 100%;
+    height: 776px;
+
+    @media (min-width: 960px) {
+        height: 700px;
+    }
+`
+
+const FlexContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    @media (min-width: 960px) {
+        flex-direction: row;
+        justify-content: center;
+    }
+`
+
+const Image = styled.img`
+    margin-top: -76px;
+    z-index: 5;
+    border-bottom: 20px solid ${blue.base};
+
+    @media (max-width: 360px) {
+        width: 310px;
+    }
+
+    @media (min-width: 960px) {
+        margin-top: 10px;
+        margin-left: 32px;
+        height: 357px;
+        width: 320px;
+    }
+
+    @media (min-width: 1200px) {
+        margin-top: -55px;
+    }
+`
+
+const TestimonialContainer = styled.div`
+    height: 434px;
+    border: 2px solid ${orange.base};
+    background-color: ${orange.lightest};
+    margin-top: -68px;
+    flex-direction: column;
+    width: 360px;
+
+    @media (max-width: 360px) {
+        width: 310px;
+    }
+
+    @media (min-width: 600px) {
+        width: 400px;
+    }
+
+    @media (min-width: 960px) {
+        height: 320px;
+        width: 520px;
+        margin-left: -80px;
+        margin-top: 240px;
+    }
+
+    @media (min-width: 960px) {
+        height: 400px;
+        margin-top: 180px;
+    }
+`
+
+const H1 = styled.h1`
+    color: ${orange.darkest};
+    height: 216px;
+    width: 310px;
+    font-family: "aktiv-grotesk-extended";
+    font-size: 32px;
+    font-weight: bold;
+    line-height: 36px;
+    margin: 104px 32px 0 32px;
+
+    @media (max-width: 360px) {
+        font-size: 24px;
+        margin: 104px 4px 0 4px;
+    }
+
+    @media (min-width: 600px) {
+        width: 320px;
+        font-size: 32px;
+        margin-bottom: 32px;
+    }
+
+    @media (min-width: 960px) {
+        width: 360px;
+        font-size: 32px;
+        margin-top: 48px;
+        margin-left: 110px;
+        margin-bottom: 0;
+        height: 190px;
+    }
+
+    @media (min-width: 960px) {
+        font-size: 32px;
+        margin-bottom: 17px;
+        margin-top: 56px;
+        margin-left: 120px;
+    }
+`
+
+const P = styled.p`
+    color: ${orange.dark};
+    font-family: "aktiv-grotesk-extended";
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 24px;
+    margin: 32px 32px 48px 32px;
+
+    @media (max-width: 360px) {
+        margin-left: 8px;
+    }
+
+    @media (min-width: 600px) {
+        font-size: 22px;
+    }
+
+    @media (min-width: 960px) {
+        margin-top: 0;
+        margin-left: 110px;
+    }
+
+    @media (min-width: 960px) {
+        margin-top: 50px;
+        margin-left: 120px;
+    }
+`
+
+function Testimonial() {
+  const {
+    prismicBootcampPrimer: {
+      data: {
+        testimonial_text: {
+          text
+        },
+        testimonial_image: {
+          url: kevinImg
+        },
+        testimonial_cite: {
+          text: cite
+        }
+      }
+    }
+  } = useStaticQuery(graphql`
+    {
+      prismicBootcampPrimer {
+        data {
+          testimonial_text {
+            text
+          }
+          testimonial_image {
+            url
+          }
+          testimonial_cite {
+            text
+          }
+        }
+      }
+    }
+  `)
+    return (
+        <Container>
+            <WhiteBlock />
+            <OrangeBlock>
+                <FlexContainer>
+                    <Image src={ kevinImg } alt="V School Grad Kevin Bowen" />
+                    <TestimonialContainer>
+                        <H1>"{ text }"</H1>
+                        <P>{ cite }</P>
+                    </TestimonialContainer>
+                </FlexContainer>
+            </OrangeBlock>
+        </Container>
+    )
+}
+
+export default Testimonial
