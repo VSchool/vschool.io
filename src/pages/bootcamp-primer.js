da1b9e1a9dd52858df8f645a4c456ed28817d634
Bob Ziroll <elderziroll@gmail.com>
2020-01-15 10:30:36 -0700

    Collected components together into index pages

--- src/pages/bootcamp-primer.js
+++ src/pages/bootcamp-primer.js
@@ -1,47 +1,48 @@
 import React from "react"
-import Layout from '../components/layout.js'
-import SEO from '../components/seo.js'
-import HeroHeader from '../components/bootcamp-primer/HeroHeader.js'
-import InfoForm from '../components/bootcamp-primer/InfoForm.js'
-import CourseBullets from '../components/bootcamp-primer/CourseBullets.js'
-import HeroImage from '../components/bootcamp-primer/HeroImage.js'
-import AboutPrimer from '../components/bootcamp-primer/AboutPrimer.js'
-import Testimonial from '../components/bootcamp-primer/Testimonial.js'
-import ContactUs from '../components/bootcamp-primer/ContactUs.js'
-import Courses from '../components/bootcamp-primer/Courses.js'
+import Layout from "../components/layout.js"
+import SEO from "../components/seo.js"
+import {
+    AboutPrimer,
+    ContactUs,
+    Courses,
+    CourseBullets,
+    HeroHeader,
+    HeroImage,
+    InfoForm,
+    Testimonial,
+} from "../components/bootcamp-primer"
+
 import { useStaticQuery, graphql } from "gatsby"
 
-export default function BootcampPrimerPage(){
-  const {
-    prismicBootcampPrimer: {
-      data: {
-        main_page_title: {
-          text: title
+export default function BootcampPrimerPage() {
+    const {
+        prismicBootcampPrimer: {
+            data: {
+                main_page_title: { text: title },
+            },
+        },
+    } = useStaticQuery(graphql`
+        {
+            prismicBootcampPrimer {
+                data {
+                    main_page_title {
+                        text
+                    }
+                }
+            }
         }
-      }
-    }
-  } = useStaticQuery(graphql`
-  {
-    prismicBootcampPrimer {
-      data {
-        main_page_title {
-          text
-        }
-      }
-    }
-  }
-`)
-  return (
-    <Layout>
-      <SEO title={title}/>
-      <HeroHeader />
-      <InfoForm />
-      <CourseBullets />
-      <HeroImage />
-      <AboutPrimer />
-      <Testimonial />
-      <ContactUs />
-      <Courses />
-    </Layout>
-  )
+    `)
+    return (
+        <Layout>
+            <SEO title={title} />
+            <HeroHeader />
+            <InfoForm />
+            <CourseBullets />
+            <HeroImage />
+            <AboutPrimer />
+            <Testimonial />
+            <ContactUs />
+            <Courses />
+        </Layout>
+    )
 }
