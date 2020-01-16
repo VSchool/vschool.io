fc92390132f36f9310316f9fa8e9ca1270e623b1
Bob Ziroll <elderziroll@gmail.com>
2020-01-15 23:53:19 -0700

    Caret added to top of submenu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,44 @@
 import React from "react"
 import styled from "styled-components"
-import { useStaticQuery, graphql } from "gatsby"
-import { gray } from "@vschool/lotus"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { gray, Button } from "@vschool/lotus"
 
+import logo from "../../images/VS_Logo_RGB.png"
 import PrimaryNavItem from "./PrimaryNavItem"
 
-const Nav = styled.ul`
+const Nav = styled.nav`
     display: flex;
     align-items: center;
     background-color: ${gray.lighter};
     margin: 0;
+    padding: 0 88px;
+    height: 80px;
+`
+
+const Ul = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 32px;
+    width: 100%;
+
+    & > img {
+        margin-right: auto;
+    }
 `
 
 // Change this to img when we have a logo ready
-const Logo = styled.p`
+const Logo = styled.img`
     margin: 0;
     margin-right: auto;
+    max-height: 100%;
+`
+
+const ApplyButton = styled(Button)`
+    font-family: "aktiv-grotesk-extended";
+    font-size: 12px;
+    letter-spacing: 0.86px;
+    line-height: 16px;
 `
 
 function Navbar() {
@@ -43,12 +66,19 @@ function Navbar() {
                             }
                         }
                     }
+                    button_text
+                    button_link {
+                        url
+                    }
                 }
             }
         }
     `)
-    const { nav } = data.prismicNavigationBar.data
-    console.log(nav)
+    const {
+        nav,
+        button_text: buttonText,
+        button_link: { url: buttonLink },
+    } = data.prismicNavigationBar.data
 
     const navItems = nav.map(item => (
         <PrimaryNavItem key={item.id} data={item} />
@@ -56,8 +86,15 @@ function Navbar() {
 
     return (
         <Nav>
-            <Logo>Navbar Logo Here</Logo>
-            {navItems}
+            <Ul>
+                <Logo src={logo} />
+                {navItems}
+                <Link to={buttonLink}>
+                    <ApplyButton buttonStyle="primary-dark" buttonSize="sm">
+                        {buttonText}
+                    </ApplyButton>
+                </Link>
+            </Ul>
         </Nav>
     )
 }
