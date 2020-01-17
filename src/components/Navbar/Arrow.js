300581b2684bedfd0105658c5523404cd62d267a
Bob Ziroll <elderziroll@gmail.com>
2020-01-17 15:50:35 -0700

    Finish desktop nav styles

--- src/components/Navbar/Arrow.js
+++ src/components/Navbar/Arrow.js
@@ -6,9 +6,10 @@ const Svg = styled.svg`
     width: 20px;
 `
 
-export default () => {
+export default ({className}) => {
     return (
         <Svg
+            className={className}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             width="24"
