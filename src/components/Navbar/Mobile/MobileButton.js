2c1d109d70a0468da2a20672873467b8c96c784a
Bob Ziroll <elderziroll@gmail.com>
2020-01-23 12:30:17 -0700

    Prep complete. Going to work on react spring next.

---
+++ src/components/Navbar/Mobile/MobileButton.js
@@ -0,0 +1,31 @@
+import React from "react"
+import styled from "styled-components"
+import { black } from "@vschool/lotus"
+
+const Button = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 20px;
+    width: 24px;
+    box-sizing: border-box;
+    cursor: pointer;
+`
+
+const Bar = styled.div`
+    width: 100%;
+    height: 4px;
+    background-color: ${black};
+`
+
+function MobileButton({toggleOpen}) {
+    return (
+        <Button onClick={toggleOpen}>
+            <Bar />
+            <Bar />
+            <Bar />
+        </Button>
+    )
+}
+
+export default MobileButton
