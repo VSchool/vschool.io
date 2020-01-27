da29cc4f2ef929ed3b4a1075b3a4a29ddb5faec6
Bob Ziroll <elderziroll@gmail.com>
2020-01-27 16:36:56 -0700

    Fix clickable area on mobile links

--- src/components/Navbar/Mobile/MobilePrimaryMenuItem.js
+++ src/components/Navbar/Mobile/MobilePrimaryMenuItem.js
@@ -8,13 +8,13 @@ import { NavbarContext } from "../navbarContext"
 
 const MenuItem = styled.li`
     list-style: none;
-    padding: 24px;
     font-size: 18px;
     line-height: 18px;
     color: ${gray.darker};
     font-weight: 800;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     border-bottom: 1px solid ${gray.light};
 
     &:first-child {
@@ -22,9 +22,13 @@ const MenuItem = styled.li`
     }
 `
 
-const ItemLink = styled(Link)`
+const Item = styled.span`
+    padding: 24px;
     text-decoration: none;
     color: inherit;
+    display: block;
+    width: 100%;
+    cursor: pointer;
 `
 
 const StyledArrow = styled(Arrow)`
@@ -34,13 +38,13 @@ const StyledArrow = styled(Arrow)`
 function MobilePrimaryMenuItem({ data }) {
     const { toggleSubMenu } = useContext(NavbarContext)
     return (
-        <MenuItem onClick={() => !data.primary.link && toggleSubMenu()}>
+        <MenuItem>
             {data.primary.link ? (
-                <ItemLink to={data.primary.link.url}>
+                <Item as={Link} to={data.primary.link.url}>
                     {data.primary.label.text}
-                </ItemLink>
+                </Item>
             ) : (
-                <span>{data.primary.label.text}</span>
+                <Item onClick={toggleSubMenu}>{data.primary.label.text}</Item>
             )}
 
             {data.items.length > 0 && <StyledArrow />}
