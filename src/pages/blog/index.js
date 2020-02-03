dd2c3fa663c012bb73b8c48c67c0523ae435ad75
Bob Ziroll <elderziroll@gmail.com>
2020-02-03 15:32:51 -0700

    Add basic detail to blog index page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,18 @@
 import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
 
 function BlogList(props) {
-    return <h1>This is a list of blogs</h1>
+    return (
+        <Layout>
+            <SEO title="Blog" />
+            <h1>This is a list of blogs</h1>
+        </Layout>
+    )
 }
 
 export default BlogList
+
+// Insert a page query here to get a list of the blogs.
+// Probably want to sort them by ascending date
+// Then map over them with their URLs to Link to them.
