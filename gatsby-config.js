312f211d996959b2619dd242af36db920e65e990
Bob Ziroll <elderziroll@gmail.com>
2020-02-18 10:37:46 -0700

    Add FB Pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,12 @@ module.exports = {
                 defaultDataLayer: { platform: "gatsby" },
             },
         },
+        {
+            resolve: `gatsby-plugin-facebook-pixel`,
+            options: {
+                pixelId: process.env.FACEBOOK_PIXEL_ID,
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
