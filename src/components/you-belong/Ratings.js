9f4d8edd6321f0ddd49732e706d357dd31290125
Nate Jensen <nathan.jensen.58@gmail.com>
2020-02-25 17:08:01 -0700

    update border color

--- src/components/you-belong/Ratings.js
+++ src/components/you-belong/Ratings.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { gray, black, green, pink} from "@vschool/lotus"
+import { black, blue, pink } from "@vschool/lotus"
 
 const Container = styled.div`
     background-color: ${pink.lightest};
@@ -35,102 +35,99 @@ const Container = styled.div`
 `
 
 const H4 = styled.h4`
-  background-color: ${pink.lightest};
-	color: ${black};
-	font-family: "aktiv-grotesk";
-	font-size: 24px;
-	font-weight: 800;
-  display: inline-block;
-  text-align: center;
-  width: 250px;
-  z-index: 3;
+    background-color: ${pink.lightest};
+    color: ${black};
+    font-family: "aktiv-grotesk";
+    font-size: 24px;
+    font-weight: 800;
+    display: inline-block;
+    text-align: center;
+    width: 250px;
+    z-index: 3;
 
-  @media(max-width: 400px){
-    font-size: 22px;
-  }
+    @media (max-width: 400px) {
+        font-size: 22px;
+    }
 
-  @media(min-width: 500px){
-    width: 300px;
-  }
+    @media (min-width: 500px) {
+        width: 300px;
+    }
 
-  @media(min-width: 600px){
-    width: 400px;
-  }
+    @media (min-width: 600px) {
+        width: 400px;
+    }
 
-  @media(min-width: 900px){
-    width: 642px;
-  }
-
+    @media (min-width: 900px) {
+        width: 642px;
+    }
 `
 
 const RatingsContainer = styled.div`
-  border: 2px solid ${green.light};
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 24px;
-  padding-top: 56px;
-  padding-bottom: 32px;
-  margin-top: -48px;
-
-  @media(min-width: 400px){
-    width: 350px;
-  }
-
-  @media(min-width: 500px){
-    width: 400px;
-  }
-
-  @media(min-width: 600px){
-    width: 500px;
-    margin-top: -32px;
-    padding-top: 48px;
-  }
-
-  @media(min-width: 900px){
-    width: 100%;
-    max-width: 850px;
-    margin-top: -16px;
-    flex-direction: row;
-    justify-content: space-between;
+    border: 2px solid ${blue.base};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
     padding-top: 56px;
-    padding-bottom: 56px;
-    padding-left: 40px;
-    padding-right: 40px;
-  }
+    padding-bottom: 32px;
+    margin-top: -48px;
+
+    @media (min-width: 400px) {
+        width: 350px;
+    }
+
+    @media (min-width: 500px) {
+        width: 400px;
+    }
+
+    @media (min-width: 600px) {
+        width: 500px;
+        margin-top: -32px;
+        padding-top: 48px;
+    }
+
+    @media (min-width: 900px) {
+        width: 100%;
+        max-width: 850px;
+        margin-top: -16px;
+        flex-direction: row;
+        justify-content: space-between;
+        padding-top: 56px;
+        padding-bottom: 56px;
+        padding-left: 40px;
+        padding-right: 40px;
+    }
 `
 
 const Image = styled.img`
-  width: 100%;
-  max-width: 230px;
-  margin: 16px 0;
-
-  @media(min-width: 400px){
-    max-width: 280px;
-  }
-
-  @media(min-width: 600px){
-    max-width: 300px;
-  }
-
-  @media(min-width: 900px){
     width: 100%;
-    max-width: 229px;
-  }
+    max-width: 230px;
+    margin: 16px 0;
 
+    @media (min-width: 400px) {
+        max-width: 280px;
+    }
+
+    @media (min-width: 600px) {
+        max-width: 300px;
+    }
+
+    @media (min-width: 900px) {
+        width: 100%;
+        max-width: 229px;
+    }
 `
 
-export default function Ratings(props){
-  const {
-    header,
-    ratings
-  } = props
-  return (
-    <Container>
-      <H4>{header}</H4>
-      <RatingsContainer>
-        { ratings.map(({rating_img} )=> <Image key={rating_img.url} src={rating_img.url}/>) } 
-      </RatingsContainer>
-    </Container>
-  )
+export default function Ratings(props) {
+    const { header, ratings } = props
+    return (
+        <Container>
+            <H4>{header}</H4>
+            <RatingsContainer>
+                {ratings.map(({ rating_img }) => (
+                    <Image key={rating_img.url} src={rating_img.url} />
+                ))}
+            </RatingsContainer>
+        </Container>
+    )
 }
