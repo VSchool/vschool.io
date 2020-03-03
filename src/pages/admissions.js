d3f7c460e2a7ab5ca989b786fe27a5d029f044af
Nate Jensen <nathan.jensen.58@gmail.com>
2020-03-02 18:32:01 -0700

    all pages source their own info now

--- src/pages/admissions.js
+++ src/pages/admissions.js
@@ -2,7 +2,8 @@ import React from "react"
 import Layout from "../components/layout.js"
 import SEO from "../components/seo.js"
 import { graphql } from "gatsby"
-import { Header, ISA, PaymentOptions } from "../components/admissions"
+import { Header } from "../components/admissions"
+import { ISA, PaymentOptions } from "../components/shared"
 
 export default function Admissions({ data }) {
     const {
@@ -11,6 +12,15 @@ export default function Admissions({ data }) {
         financing_header: { text: header },
         financing_btn_link: { url: link },
         financing_btn: { text: btnText },
+        isa_sub2: { text: isaSub2 },
+        isa_sub1: { text: isaSub1 },
+        isa_learn_more_link: { url: isaLink },
+        isa_learn_more_btn: { text: isaBtnText },
+        isa_info1: { text: isaInfo1 },
+        isa_info2: { text: isaInfo2 },
+        isa_header: { text: isaHeader },
+        isa_image: { url: isaImg },
+        payment_options,
     } = data.prismicIsaPage.data
     return (
         <Layout>
@@ -22,8 +32,17 @@ export default function Admissions({ data }) {
                 link={link}
                 heroImg={heroImg}
             />
-            <ISA />
-            <PaymentOptions />
+            <ISA
+                header={isaHeader}
+                sub1={isaSub1}
+                sub2={isaSub2}
+                info1={isaInfo1}
+                info2={isaInfo2}
+                img={isaImg}
+                btnText={isaBtnText}
+                link={isaLink}
+            />
+            <PaymentOptions options={payment_options} />
         </Layout>
     )
 }
@@ -46,6 +65,41 @@ export const query = graphql`
                 }
                 financing_btn {
                     text
+                }
+                payment_options {
+                    payment_type {
+                        text
+                    }
+                    payment_info_link {
+                        url
+                    }
+                    payment_info {
+                        text
+                    }
+                }
+                isa_sub2 {
+                    text
+                }
+                isa_sub1 {
+                    text
+                }
+                isa_learn_more_link {
+                    url
+                }
+                isa_learn_more_btn {
+                    text
+                }
+                isa_info1 {
+                    text
+                }
+                isa_info2 {
+                    text
+                }
+                isa_header {
+                    text
+                }
+                isa_image {
+                    url
                 }
             }
         }
