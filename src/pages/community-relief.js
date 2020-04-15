0034c3461060d9b01e1ccb800e0c4fa5b91de2fe
Nate Jensen <nathan.jensen.58@gmail.com>
2020-04-15 10:40:30 -0600

    setting-up-community-relief page

---
+++ src/pages/community-relief.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout.js"
+import SEO from "../components/seo.js"
+import { graphql } from "gatsby"
+
+export default function CommunityRelief({data}){
+  return (
+    <Layout>
+      <SEO title="Community Relief"/>
+
+    </Layout>
+  )
+}
