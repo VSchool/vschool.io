c256f49f9682adf3c70f7ad5e9fbb736052e098a
Bob Ziroll <elderziroll@gmail.com>
2020-04-17 13:52:38 -0600

    About to revert changing ALL anchors AND links to QueryLink

--- src/components/shared/QueryLink.js
+++ src/components/shared/QueryLink.js
@@ -1,21 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-import { useQueryString } from "../../hooks/useQueryString"
 
-// Custom component that adds the site's current query string
-// to all links so they're maintained around the site
-// When importing, still use `import Link from "../path/to/shared/QueryLink"`
-function QueryLink({ to, children, ...rest }) {
-    const query = useQueryString()
-    return to.startsWith("/") ? (
-        <Link {...rest} to={`${to}${query}`}>
-            {children}
-        </Link>
-    ) : (
+// A replacement for regular anchor elements that automatically implements
+// the querystring from sessionStorage
+// This makes it so we can track the UTM parameters on sites outside of our own
+// like on links to Calendly.
+export default function({ to, children, ...rest }) {
+    const query = sessionStorage.getItem("query")
+    return (
         <a {...rest} href={`${to}${query}`}>
             {children}
         </a>
     )
 }
-
-export default QueryLink
