57dbe78169e0347e61b7f1f766f7b00614273fbe
Nate Jensen <nathan.jensen.58@gmail.com>
2020-05-04 17:24:29 -0600

    blog-page-started

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,17 +1,41 @@
 import React from "react"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import { BlogNav, BlogList } from "../../components/blog"
+import { graphql } from "gatsby"
 
-function BlogList(props) {
+export default function Blog({ data }) {
+    const { edges } = data.allGhostPost
     return (
         <Layout>
             <SEO title="Blog" />
-            <h1>This is a list of blogs</h1>
+            <BlogNav />
+            <BlogList posts={edges} />
         </Layout>
     )
 }
 
-export default BlogList
+export const query = graphql`
+    {
+        allGhostPost {
+            edges {
+                node {
+                    title
+                    slug
+                    published_at(formatString: "MMMM DD, YYYY")
+                    feature_image
+                    url
+                    authors {
+                        slug
+                        name
+                        profile_image
+                        cover_image
+                    }
+                }
+            }
+        }
+    }
+`
 
 // Insert a page query here to get a list of the blogs.
 // Probably want to sort them by ascending date
