3bfd30b0288c0b628f1ead33272541761845d86a
Nate Jensen <nathan.jensen.58@gmail.com>
2020-05-06 11:37:09 -0600

    inital blog layout setup

--- src/components/blog/BlogList.js
+++ src/components/blog/BlogList.js
@@ -1,17 +1,25 @@
 import React from "react"
 import styled from "styled-components"
 import PostPreview from "./PostPreview.js"
+import TopPostPreview from "./TopPostPreview.js"
 
 const PageContainer = styled.div`
     padding: 0 16px;
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-items: center;
+
+    @media(min-width: 400px){
+        grid-template-columns: 1fr 1fr;
+    }
 `
 
 export default function BlogList(props) {
     const { posts } = props
     return (
         <PageContainer>
-            <h1>This is a list of blogs</h1>
-            {posts.map(({ node }) => (
+            <TopPostPreview {...posts[0].node} />
+            {posts.slice(1).map(({ node }) => (
                 <PostPreview key={node.id} {...node} />
             ))}
         </PageContainer>
