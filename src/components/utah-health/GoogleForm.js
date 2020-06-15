e365467d85bbef4927842380c5aebed64499ddcd
Nate Jensen <nathan.jensen.58@gmail.com>
2020-06-15 09:25:47 -0600

    all pages refactored

--- src/components/utah-health/GoogleForm.js
+++ src/components/utah-health/GoogleForm.js
@@ -3,35 +3,14 @@ import styled from "styled-components"
 import QueryLink from "../shared/QueryLink.js"
 import { gray, black, Button } from "@vschool/lotus"
 
-const Container = styled.div`
+const Container = styled.section`
     background-color: ${gray.lighter};
-    padding-left: 24px;
-    padding-right: 24px;
     padding-bottom: 96px;
     padding-top: 16px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     justify-content: center;
-
-    @media (min-width: 600px) {
-        padding-left: 32px;
-        padding-right: 32px;
-    }
-
-    @media (min-width: 840px) {
-        padding-left: 40px;
-        padding-right: 40px;
-    }
-
-    @media (min-width: 1200px) {
-        padding-left: 88px;
-        padding-right: 88px;
-    }
 `
 
 const Header = styled.h2`
-    font-family: "aktiv-grotesk";
     font-weight: 800;
     font-size: 24px;
     line-height: 30px;
