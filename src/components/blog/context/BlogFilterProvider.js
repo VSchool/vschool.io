edfd671b92b5748d539101200a7cec0f4fc504bf
Bob Ziroll <elderziroll@gmail.com>
2020-08-18 16:25:40 -0600

    Remove console.logs

--- src/components/blog/context/BlogFilterProvider.js
+++ src/components/blog/context/BlogFilterProvider.js
@@ -1,14 +1,9 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 
 export const BlogFilterContext = React.createContext()
 
 export default function BlogFilterProvider(props) {
     const [blogFilter, setBlogFilter] = useState("all")
-
-    // Just for dev work. Can delete whole effect when ready
-    useEffect(() => {
-        console.log("current blogFilter:", blogFilter)
-    }, [blogFilter])
 
     return (
         <BlogFilterContext.Provider value={{ blogFilter, setBlogFilter }}>
