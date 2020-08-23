35a39e5c8fa27918f4ac14392fbf2323e5cceb5a
Bob Ziroll <elderziroll@gmail.com>
2020-08-22 22:30:58 -0600

    navbar fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ const Nav = styled.nav`
     padding: 28px 16px;
     width: 100%;
     height: 80px;
-    /* z-index: 999; */
+    z-index: 999;
     box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
 
     @media (min-width: 1000px) {
