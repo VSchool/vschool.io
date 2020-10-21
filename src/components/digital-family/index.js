dc85c21638bb7adbb80760a1ea8482eb35813766
Bob Ziroll <elderziroll@gmail.com>
2020-10-21 16:46:12 -0600

    AlumniCompanies2 working. Replacing old instances

---
+++ src/components/digital-family/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Hero from "./Hero"
+import Explainer from "./Explainer"
+import Members from "./Members"
+
+export default function index() {
+    return (
+        <>
+            <Hero />
+            <Explainer />
+            <Members />
+        </>
+    )
+}
