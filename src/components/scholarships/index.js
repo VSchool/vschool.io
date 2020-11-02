921f92bef9992675c5bb0fa5d67fa9ce0f05325b
Bob Ziroll <elderziroll@gmail.com>
2020-11-02 16:44:47 -0700

    Page finished with minor changes to be made

--- src/components/scholarships/index.js
+++ src/components/scholarships/index.js
@@ -1,11 +1,18 @@
 import React from "react"
 import Hero from "./Hero"
 import CurrentScholarships from "./CurrentScholarships"
+import Testimonial from "./Testimonial"
+import Courses from "./Courses"
+import DigitalFamily from "./DigitalFamily"
+
 export default function index() {
     return (
         <>
             <Hero />
             <CurrentScholarships />
+            <Testimonial />
+            <Courses />
+            <DigitalFamily />
         </>
     )
 }
