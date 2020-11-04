149e18cea8503250291f295788d81ea68ca507b5
Bob Ziroll <elderziroll@gmail.com>
2020-11-04 10:18:31 -0700

    Completed the stages section

--- src/components/scholarship/index.js
+++ src/components/scholarship/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Hero from "./Hero"
+import Stages from "./Stages"
 
-export default function Scholarship() {
+export default function Scholarship({ data }) {
     return (
         <div>
-            <Hero />
+            <Hero {...data} />
+            <Stages />
         </div>
     )
 }
