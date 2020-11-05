ae33afa41c6446ee104a71189a7adb3a226deeef
Michael Stedman <michaelstedman81@gmail.com>
2020-11-05 14:32:36 -0700

    Refactored Header Component
    Refactored comonent to do its own GraphQL queries.

--- src/components/careers/index.js
+++ src/components/careers/index.js
@@ -1,4 +1,13 @@
-export { default as Header } from "./Header.js"
+import React from 'react'
+import Header from "./Header.js"
 export { default as HeroImage } from "./HeroImage.js"
 export { default as Positions } from "./Positions.js"
 export { default as SubmitAResume } from "./SubmitAResume.js"
+
+export default function CareersPage() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
