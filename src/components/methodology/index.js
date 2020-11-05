327db8febd73135747ce17f7e0a6483d30c015d7
Michael Stedman <michaelstedman81@gmail.com>
2020-11-05 12:55:25 -0700

    Moved Pathways Component Import
    Imported Pathways component into the methodology index.js.

--- src/components/methodology/index.js
+++ src/components/methodology/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import HeroHeader from "./HeroHeader.js"
 import Testimonial1 from "./Testimonial1.js"
 import Differences from "./Differences.js"
+import PathwaysHeader from "./PathwaysHeader.js"
+import Pathways from "./pathways"
 export { default as Testimonial2 } from "./Testimonial2.js"
 export { default as SchoolComparisons } from "./SchoolComparisons.js"
 export { default as CTA } from "./CTA.js"
-export { default as Pathways } from "./pathways"
-export { default as PathwaysHeader } from "./PathwaysHeader.js"
 
 export default function ComparesPage() {
     return (
@@ -14,6 +14,8 @@ export default function ComparesPage() {
             <HeroHeader />
             <Testimonial1 />
             <Differences />
+            <PathwaysHeader />
+            <Pathways />
        </>
     )
 }
