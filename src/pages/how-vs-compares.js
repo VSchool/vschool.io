b83d487d14c1b77e1552622a56e8f8f9822ac5f8
Michael Stedman <michaelstedman81@gmail.com>
2020-11-13 08:22:36 -0700

    Removed Imports
    Removed the last of the GraphQL queries from the orginal pages. Just needed to remove the imports.

--- src/pages/how-vs-compares.js
+++ src/pages/how-vs-compares.js
@@ -1,10 +1,9 @@
 import React from "react"
 import Layout from "../components/layout.js"
 import SEO from "../components/seo.js"
-import { graphql } from "gatsby"
 import ComparesPage from '../components/methodology'
 
-export default function HowVSCompares({ data }) {
+export default function HowVSCompares() {
 
     return (
         <Layout>
