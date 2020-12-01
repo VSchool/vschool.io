bd88df41cc97d1640b2ab20c4adf7db2b216db54
Bob Ziroll <elderziroll@gmail.com>
2020-12-01 10:27:32 -0700

    Adding CTA at bottom of page

--- src/components/scholarship/CTA.js
+++ src/components/scholarship/CTA.js
@@ -3,12 +3,12 @@ import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
 import { gray, blue, Button } from "@vschool/lotus"
 import Link from "../shared/QueryLink"
+import { Link as ScrollLink } from "react-scroll"
 
 const Container = styled.section`
     padding-top: 96px;
     padding-bottom: 96px;
-    background-color: ${blue.lightest};
-    margin-bottom: 96px;
+    background-color: ${props => props.theme.lightest};
 `
 
 const Title = styled.h2`
@@ -21,47 +21,10 @@ const Title = styled.h2`
     }
 `
 
-const DatesContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-bottom: 24px;
-    @media (min-width: 800px) {
-        flex-direction: row;
-    }
-`
-
-const DateGroup = styled.div`
-    text-align: center;
-    margin-bottom: 24px;
-
-    @media (min-width: 800px) {
-        &:first-child {
-            margin-right: 96px;
-        }
-    }
-`
-
-const DateText = styled.h6`
-    color: ${blue.base};
-    font-size: 14px;
-    line-height: 20px;
-    margin-bottom: 4px;
-
-    @media (min-width: 800px) {
-        font-size: 16px;
-        line-height: 24px;
-        margin-bottom: 8px;
-    }
-`
-const Date = styled.h5`
-    color: ${gray.darker};
-`
-
 const Text = styled.p`
     font-size: 16px;
-    /* line-height: 24px; */
     margin-bottom: 24px;
+    text-align: center;
     color: ${gray.darker};
 
     @media (min-width: 800px) {
@@ -83,9 +46,6 @@ export default function CTA(props) {
                     cta_button_text {
                         text
                     }
-                    cta_button_link {
-                        url
-                    }
                 }
             }
         }
@@ -95,33 +55,21 @@ export default function CTA(props) {
         cta_title: { text: title },
         cta_text: { text },
         cta_button_text: { text: buttonText },
-        cta_button_link: { url: buttonLink },
     } = data.prismicScholarshipPageSharedData.data
 
     const {
-        winner_announced_text: { text: winnerText },
-        winner_announced_date: winnerDate,
-        deadline_text: { text: deadlineText },
-        deadline_date: deadlineDate,
+        primary_theme_color: primaryThemeColor,
+        secondary_theme_color: secondaryThemeColor,
     } = props
+    console.log(primaryThemeColor)
 
     return (
         <Container>
             <Title>{title}</Title>
-            <DatesContainer>
-                <DateGroup>
-                    <DateText>{deadlineText}</DateText>
-                    <Date>{deadlineDate}</Date>
-                </DateGroup>
-                <DateGroup>
-                    <DateText>{winnerText}</DateText>
-                    <Date>{winnerDate}</Date>
-                </DateGroup>
-            </DatesContainer>
             <Text>{text}</Text>
-            <Link to={buttonLink}>
+            <ScrollLink to="application-form" smooth={true} offset={-320}>
                 <Button size="lg">{buttonText}</Button>
-            </Link>
+            </ScrollLink>
         </Container>
     )
 }
