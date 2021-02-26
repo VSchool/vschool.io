8d8ef73306cbe2febb482fcaee5c69ecfffcd433
Bob Ziroll <elderziroll@gmail.com>
2021-02-26 09:37:45 -0700

    Build women up

--- src/components/building-women-up/index.js
+++ src/components/building-women-up/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Hero from "./Hero"
+import Scholarships from "./Scholarships"
 
 export default function BuildingWomenUp() {
     return (
         <>
             <Hero />
+            <Scholarships />
         </>
     )
 }
