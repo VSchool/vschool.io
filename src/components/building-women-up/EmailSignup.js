34c62050b9adb3cfda78d1f90a9c7c3cca5e6363
Bob Ziroll <elderziroll@gmail.com>
2021-03-01 15:27:43 -0700

    Email signup working

--- src/components/building-women-up/EmailSignup.js
+++ src/components/building-women-up/EmailSignup.js
@@ -1,9 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
+import ConvertKitSubscribe from "../shared/ConvertKitSubscribe"
 
 const Container = styled.section`
-    padding-top: 96px;
+    padding: 96px 16px 0;
+
+    @media (min-width: 800px) {
+        padding: 160px 88px 0;
+    }
 `
 const Title = styled.h3`
     text-align: center;
@@ -29,6 +34,7 @@ export default function EmailSignup() {
     return (
         <Container>
             <Title>{emailTitle}</Title>
+            <ConvertKitSubscribe tag="women scholarship" />
         </Container>
     )
 }
---
+++ src/components/shared/ConvertKitSubscribe.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+import { TextInput, Button, blue, gray } from "@vschool/lotus"
+
+const FormContainer = styled.form`
+    padding: 40px 24px;
+    background-color: ${blue.lightest};
+    border: 2px solid ${blue.base};
+    width: 100%;
+    max-width: 1024px;
+
+    @media (min-width: 800px) {
+        padding: 32px 48px;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-between;
+    }
+`
+
+const StyledInput = styled(TextInput)`
+    margin-bottom: 16px;
+
+    @media (min-width: 800px) {
+        margin-bottom: 0;
+        margin-right: 16px;
+        flex-basis: 170px;
+        flex-grow: 1;
+    }
+`
+
+const StyledButton = styled(Button)`
+    width: 100%;
+    flex-basis: 170px;
+    flex-grow: 1;
+`
+
+const Disclaimer = styled.p`
+    flex-basis: 100%;
+    margin-top: 24px;
+    font-size: 14px;
+    line-height: 18px;
+    color: ${gray.darker};
+    font-weight: normal;
+    text-align: center;
+
+    @media (min-width: 800px) {
+        text-align: left;
+    }
+`
+
+export default function ConvertKitSubscribe({ tag, ...props }) {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const data = JSON.stringify({ name, email, tag })
+        const options = {
+            method: "POST",
+            body: data,
+        }
+
+        fetch(
+            process.env.GATSBY_CONVERTKIT_SIGNUP_ZAPIER_WEBHOOK_URL,
+            options
+        ).then(res => console.log(res))
+    }
+
+    return (
+        <FormContainer {...props} onSubmit={handleSubmit}>
+            <StyledInput
+                type="text"
+                label="First Name"
+                name="name"
+                placeholder="Your First Name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+                validationText="Required"
+            />
+            <StyledInput
+                type="email"
+                label="Email"
+                name="email"
+                placeholder="Your Email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+                validationText="Required"
+            />
+            <StyledButton>Sign Up</StyledButton>
+            <Disclaimer>
+                By submitting your information you are agreeing to receive
+                emails from vschool.io.
+            </Disclaimer>
+        </FormContainer>
+    )
+}
