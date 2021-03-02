3b86171124920b6db8961bd101f81104f6bc6cef
Bob Ziroll <elderziroll@gmail.com>
2021-03-02 10:14:15 -0700

    Update link styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 import Link from "./shared/QueryLink"
 import { useLocation } from "@reach/router"
 import Navbar from "./Navbar"
