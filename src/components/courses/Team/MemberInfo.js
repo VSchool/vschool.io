b6505640c7a673f40858d0390c95ee279a5313a5
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-21 23:12:47 -0500

    Finished Team section
    Finished styling team section and fixed the last of the desktop width resizing.

--- src/components/courses/Team/MemberInfo.js
+++ src/components/courses/Team/MemberInfo.js
@@ -3,76 +3,63 @@ import styled from 'styled-components'
 import { gray } from '@vschool/lotus'
 
 const InfoContainer = styled.div`
-    // border: 1px solid blue;
-
     @media (min-width: 800px) {
         width: 232px;
-        // border: 1px solid green;
     }
 
     & > .image-container {
-        margin: 0px 0px 32px 0px;
         display: flex;
         justify-content: center;
         align-items: center;
-        // border: 1px solid green;
+        margin-bottom: 32px;
 
         & > img {
             width: 128px;
             height: 128px;
 
             @media (min-width: 800px) {
-                width: 128px;
                 height: 132px;
             }
         }
     }
 
     & > .team-name {
-        margin: 0px 0px 4px 0px;
-        font-family: Aktiv Grotesk;
+        margin-bottom: 4px;
+        color: ${gray.darkest};
         font-style: normal;
         font-weight: 800;
         font-size: 16px;
         line-height: 24px;
         text-align: center;
-        color: ${gray.darkest};
-        // border: 1px dotted orange;
 
         @media (min-width: 800px) {
-            // margin: 0px 16px 0px 16px;
-            font-family: Aktiv Grotesk;
-            font-style: normal;
-            font-weight: 800;
             font-size: 20px;
-            line-height: 24px;
-            text-align: center;
-            color: ${gray.darkest};
         }
     }
 
+    // *** team-role - Removing the font info from this class will change the font to something else
+    // Need to find where the global styling is set at to find out what it is setting
+    // for styling and I can work from that
+
     & > .team-role {
-        margin: 0px;
         height: 32px;
-        font-family: Aktiv Grotesk Ex;
+        margin: 0px;
+        color: ${gray.darkest};
+        font-family: 'Aktiv Grotesk Ex';
         font-style: normal;
         font-weight: bold;
         font-size: 12px;
         line-height: 16px;
         text-align: center;
         letter-spacing: 0.5px;
-        color: ${gray.darkest};
-        // border: 1px dotted orange;
     }
 `
 
 const TeamLogo = styled.div`
+    height: 66px;
     margin: 16px 0px 0px 0px;
-    min-height: 66px;
     background-position: center;
     background-repeat: no-repeat;
-    height: 58px;
-    // border: 1px dotted orange;
 `        
 
 export default function MemberInfo(props) {
