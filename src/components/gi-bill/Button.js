a75c01461c4d35921b8bc778b455b3d65cb20974
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-22 01:53:39 -0500

    Commit

---
+++ src/components/gi-bill/Button.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { gray } from '@vschool/lotus'
+
+const StyledButton = styled.button`
+    display: flex;
+    align-items: center;
+    padding: 20px 44px 20px 44px;
+    height: 56px;
+    border: 2px solid ${gray.darkest};
+    color: ${gray.lightest};
+    background: ${gray.darker};
+    font-family: Aktiv Grotesk Ex;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 14px;
+    line-height: 16px;
+    text-align: center;
+    letter-spacing: 1px;
+`
+
+export default function Button(props) {
+    const { buttonText, className } = props
+    console.log(gray)
+
+    return <StyledButton className={className}>{buttonText}</StyledButton>
+}
--- src/components/gi-bill/index.js
+++ src/components/gi-bill/index.js
@@ -4,7 +4,11 @@ import FormSlice from './FormSlice'
 export default function GiBillPage() {
     return (
         <>
-            <FormSlice />
-        </>
+            <FormSlice 
+                sliceHeader={{heading: 'Online Coding Bootcamp for Veterans', subHeading: 'VA Approved'}}
+                formHeading={'Use your G.I Bill benefits to learn to code and land a job in tech. Fill out the form to learn more!'}
+                buttonText={'Request Information'}
+            />
+        </> 
     )
 }
