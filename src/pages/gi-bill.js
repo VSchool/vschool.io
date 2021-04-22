7138cb09764918cf9c7ff67eeb2338ba794c6613
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-22 00:26:26 -0500

    Started page

---
+++ src/pages/gi-bill.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import GiBillPage from '../components/gi-bill'
+
+export default function GiBill() {
+    return (
+        <Layout>
+            <SEO title={'GI Bill Coding Bootcamp'} />
+            <GiBillPage />
+        </Layout>
+    )
+}
