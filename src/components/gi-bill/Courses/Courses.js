8bc74bf094100725b857492178071b3ec309f1b8
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-25 17:44:34 -0500

    Commit

---
+++ src/components/gi-bill/Courses/Courses.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CoursesContainer = styled.div`
+    margin-top: 112px;
+    border: 2px dashed black;
+`
+
+export default function Courses() {
+    return (
+        <CoursesContainer>
+            <p>Courses</p>
+        </CoursesContainer>
+    )
+}
