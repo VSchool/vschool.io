81810052c5c1a9a72c78dbfb26bcbf4148d597a3
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-25 15:26:58 -0500

    Commit

---
+++ src/components/gi-bill/Outcomes/Outcomes.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import styled from 'styled-components'
+import StatsBar from './StatsBar'
+import { gray, blue } from '@vschool/lotus'
+
+const OutcomesContainer = styled.div`
+    margin-top: 112px;
+    display: grid;
+    grid-template-columns: [left-margin-start] 64px [left-margin-end col1-start] 1fr [col1-end col2-start] 1fr [col2-end col3-start] 1fr [col3-end col4-start] 1fr [col4-end col5-start] 1fr [col5-end col6-start] 1fr [col6-end col7-start] 1fr [col7-end col8-start] 1fr [col8-end col9-start] 1fr [col9-end col10-start] 1fr [col10-end col11-start] 1fr [col11-end col2-start] 1fr [col12-end right-margin-start] 64px [right-margin-end];
+    grid-column-gap: 24px;
+    grid-template-rows: auto;
+    // border: 2px dashed black;
+
+    & > .title {
+        margin-bottom: 16px;
+        grid-column-start: col3-start;
+        grid-column-end: span col10-end;
+        grid-row-start: 1;
+        text-align: center;
+        // background: lightblue;
+    }
+
+    & > .description {
+        margin-bottom: 48px;
+        grid-column-start: col3-start;
+        grid-column-end: span col10-end;
+        grid-row-start: 2;
+        font-weight: normal;
+        text-align: center;
+        // background: lightgreen;
+    }
+
+    & > .stats-bar {
+        margin-bottom: 104px;
+        grid-column-start: col2-start;
+        grid-column-end: col11-end;
+        grid-row-start: 3;
+    }
+
+    & > .content-wrapper {
+        grid-column-start: left-margin-start;
+        grid-column-end: right-margin-end;
+        grid-row-start: 4;
+        display: grid;
+        grid-template-columns: 
+            [start] 88px [c1-start] 1fr [c2-start] 1fr [c3-start] 1fr [c4-start] 1fr [c5-start] 1fr [c6-start] 1fr [c7-start] 1fr [c8-start] 1fr [c9-start] 1fr [c10-start] 1fr [c11-start] 1fr [c12-start] 1fr 88px [end];
+        grid-column-gap: 24px;  
+
+        & > .sub-wrapper {
+            grid-column-start: start;
+            grid-column-end: c10-start;
+            display: grid;
+            grid-template-columns: 
+                [start] 88px [one] 1fr [two] 1fr [three] 1fr [four] 1fr [five] 1fr [six] 1fr [seven] 1fr [eight] 1fr [nine] 1fr  [ten] 1fr  [eleven] 1fr  [twelve] 1fr 88px [end];
+            grid-column-gap: 24px;
+            grid-template-rows: auto;
+            position: relative;
+            background: ${blue.darkest};
+
+            & > .sub-title {
+                margin-top: 56px;
+                grid-column-start: one;
+                grid-column-end: twelve;
+                grid-row-start: 1;
+                margin-bottom: 40px;
+                color: ${gray.light};
+                // border: 2px dotted orange;
+            }
+
+            & > .sub-description {
+                margin-bottom: 56px;
+                grid-column-start: one;
+                grid-column-end: twelve;
+                grid-row-start: 2;
+                font-weight: normal;
+                font-size: 16px;
+                line-height: 24px;
+                color: ${gray.light};
+                // border: 2px dotted orange;
+            }
+
+            & > .section-image {
+                position: absolute;
+                top: calc(50% - 320px/2);
+                right: -276px;
+                height: 320px;  
+                // border: 2px solid red;              
+            }
+        }
+    }
+`
+
+export default function Outcomes(props) {
+    const { prismicData } = props
+
+    let outcomesTitle = prismicData.outcomes_title[0].text
+    let outcomesDescription = prismicData.outcomes_description[0].text
+    let outcomesStatistics = prismicData.outcomes_statistics
+    let outcomesSubTitle = prismicData.outcomes_subtitle[0].text
+    let outcomesSubDescription = prismicData.outcomes_subdescription[0].text
+    let outcomesImage = prismicData.outcomes_image
+
+    return (
+        <OutcomesContainer>
+            <h2 className={'title'}>{outcomesTitle}</h2>
+            <p className={'description'}>{outcomesDescription}</p>
+            <StatsBar className={'stats-bar'} stats={outcomesStatistics} />
+            <div className={'content-wrapper'}>
+                <div className={'sub-wrapper'}>
+                    <h3 className={'sub-title'}>{outcomesSubTitle}</h3>
+                    <p className={'sub-description'}>{outcomesSubDescription}</p>
+                    <img className={'section-image'} src={outcomesImage.url} alt={outcomesImage.alt} />
+                </div>
+            </div>
+        </OutcomesContainer>
+    )
+}
---
+++ src/components/gi-bill/Outcomes/StatsBar.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import { blue, gray } from '@vschool/lotus'
+
+const BarContainer = styled.div`
+    padding: 24px 44px 24px 44px;
+    display: flex;
+    justify-content: center;
+    border-top: 2px solid blue;
+    border-bottom: 2px solid blue;
+
+    & > .stat-one {
+        // background: black;
+    }
+
+    & > .stat-two {
+        margin-left: 24px;
+        margin-right: 24px;
+        // background: black;
+    }
+
+    & > .stat-three {
+        // background: black;
+    }
+`
+
+const StatContainer = styled.div`
+    width: 232px;
+    // height: 112px;
+    // border: 1px dashed orange;
+
+    & > p {
+        margin-bottom: 8px;
+        height: 48px;
+        color: ${gray.darker};
+        font-weight: 800;
+        font-size: 16px;
+        line-height: 24px;
+        text-align: center;
+        letter-spacing: 0.25px;
+        text-transform: uppercase;
+        // background: lightblue;
+    }
+
+    & > h1 {
+        color: ${blue.darker};
+        font-size: 56px;
+        line-height: 56px;
+        text-align: center;
+        // background: lightgreen;
+    }
+`
+
+function Stat(props) {
+    const { title, description, className } = props
+
+    return (
+        <StatContainer className={className}>
+            <p>{title}</p>
+            <h1>{description}</h1>
+        </StatContainer>
+    )
+}
+
+export default function StatsBar(props) {
+    const { stats, className } = props
+    let statOneTitle = stats[0].outcomes_stat_title[0].text
+    let statOneDescription = stats[0].outcomes_stat_description[0].text
+    let statTwoTitle = stats[1].outcomes_stat_title[0].text
+    let statTwoDescription = stats[1].outcomes_stat_description[0].text
+    let statThreeTitle = stats[2].outcomes_stat_title[0].text
+    let statThreeDescription = stats[2].outcomes_stat_description[0].text
+
+    return (
+        <BarContainer className={className}>
+            <Stat className={'stat-one'} title={statOneTitle} description={statOneDescription} />
+            <Stat className={'stat-two'} title={statTwoTitle} description={statTwoDescription} />
+            <Stat className={'stat-three'} title={statThreeTitle} description={statThreeDescription}/>
+        </BarContainer>
+    )
+}
