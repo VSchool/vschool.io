e0f1a93e900743129710a24f486be83bdf6c5f9d
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-26 13:04:39 -0500

    Commit

--- src/components/gi-bill/Form.js
+++ src/components/gi-bill/Form.js
@@ -1,44 +1,112 @@
 import React from 'react'
-import FormInput from './FormInput'
 import styled from 'styled-components'
+import FormInput from './FormInput'
+import Button from './AdditionalResources/Button'
 
 const StyledForm = styled.form`
     display: grid;
-    grid-template-columns: [left-margin col1-start] 1fr [col1-end col2-start] 1fr [col2-end col3-start] 1fr [col3-end col4-start] 1fr [col4-end col5-start] 1fr [col5-end col6-start] 1fr [col6-end right-margin];
+    grid-template-columns: [line1] 1fr [line2] 1fr [line3] 1fr [line4] 1fr [line5] 1fr [line6] 1fr [end];
     grid-column-gap: 24px;
     grid-row-gap: 16px;
     grid-template-rows: auto;
-    border: 2px solid black;
+    // border: 2px solid black;
 
     & > .first-name {
-        grid-column-start: col1-start;
-        grid-column-end: col3-end;
+        grid-column-start: line1;
+        grid-column-end: line4;
         grid-row-start: 1;
         // background: red;
     }
 
     & > .last-name {
-        grid-column-start: col1-start;
-        grid-column-end: col3-end;
-        grid-row-start: 2;
+        grid-column-start: line4;
+        grid-column-end: end;
+        grid-row-start: 1;
         // background: blue;
+    }
+
+    & > .email {
+        grid-column-start: line1;
+        grid-column-end: end;
+        grid-row-start: 3;
+        // background: green;
+    }
+
+    & > .phone {
+        grid-column-start: line1;
+        grid-column-end: end;
+        grid-row-start: 4;
+        // background: gray;
+    }
+
+    & > .button-wrapper {
+        margin-top: 24px;
+        grid-column-start: line1;
+        grid-column-end: end;
+        grid-row-start: 5;
+        // background: lightgray;
+
+        & > .form-button {
+
+        }
     }
 `
 
 export default function Form(props) {
-    const { className } = props 
+    const { className, formData } = props
+
+    const inputsArr = [...formData.inputs.form]
+
+    let input1Label = inputsArr[0].hero_form_label[0].text
+    let input1Placeholder = inputsArr[0].hero_form_placeholder[0].text
+
+    let input2Label = inputsArr[1].hero_form_label[0].text
+    let input2Placeholder = inputsArr[1].hero_form_placeholder[0].text
+
+    let input3Label = inputsArr[2].hero_form_label[0].text
+    let input3Placeholder = inputsArr[2].hero_form_placeholder[0].text
+
+    let input4Label = inputsArr[3].hero_form_label[0].text
+    let input4Placeholder = inputsArr[3].hero_form_placeholder[0].text
+
+    let buttonText = formData.button.text[0].text
 
     return (
         <StyledForm className={className}>
             <FormInput 
                 className={'first-name'}
-                label={'Label 1'}
+                name={'firstName'}
+                label={input1Label}
+                placeholder={input1Placeholder}
+                required={true}
             />
 
             <FormInput 
                 className={'last-name'}
-                label={'Label 2'}
+                name={'lastName'}
+                label={input2Label}
+                placeholder={input2Placeholder}
+                required={true}
             />
+
+            <FormInput 
+                className={'email'}
+                name={'email'}
+                label={input3Label}
+                placeholder={input3Placeholder}
+                required={true}
+            />
+
+            <FormInput 
+                className={'phone'}
+                name={'phone'}
+                label={input4Label}
+                placeholder={input4Placeholder}
+                required={true}
+            />
+            <div className={'button-wrapper'}>
+                <Button buttonText={buttonText} className={'form-button'} />
+            </div>
         </StyledForm>
     )
 }
--- src/components/gi-bill/Hero.js
+++ src/components/gi-bill/Hero.js
@@ -4,12 +4,14 @@ import Form from './Form'
 import { gray, red, blue } from '@vschool/lotus'
 
 const HeroContainer = styled.div`
-    margin-top: 112px;
+    padding-top: 112px;
+    padding-bottom: 112px;
     display: grid;
     grid-template-columns: [left-margin] 64px [left-margin-end col1-start] 1fr [col1-end col2-start] 1fr [col2-end col3-start] 1fr [col3-end col4-start] 1fr [col4-end col5-start] 1fr [col5-end col6-start] 1fr [col6-end col7-start] 1fr [col7-end col8-start] 1fr [col8-end col9-start] 1fr [col9-end col10-start] 1fr [col10-end col11-start] 1fr [col11-end col2-start] 1fr [col12-end right-margin-start] 64px [right-margin];
     grid-column-gap: 24px;
     grid-template-rows: auto;
-    border: 2px dashed black;
+    background: ${blue.lightest};
+    // border: 2px dashed black;
 
     & > .title {
         grid-column-start: col1-start;
@@ -21,7 +23,7 @@ const HeroContainer = styled.div`
         font-size: 56px;
         line-height: 56px;
         color: ${gray.darkest};
-        border: 2px solid orange;
+        // border: 2px solid orange;
     }
 
     & > .tagline {
@@ -36,7 +38,7 @@ const HeroContainer = styled.div`
         letter-spacing: 0.25px;
         text-transform: uppercase;
         color: ${red.dark};        
-        border: 2px dotted orange;
+        // border: 2px dotted orange;
     }
 
     & > .wrapper {
@@ -48,9 +50,10 @@ const HeroContainer = styled.div`
         grid-template-columns: [margin-left] 64px [margin-left-end c1-start] 1fr [c1-end c2-start] 1fr [c2-end c3-start] 1fr [c3-end c4-start] 1fr [c4-end c5-start] 1fr [c5-end c6-start] 1fr [c6-end c7-start] 1fr [c7-end c8-start] 1fr [c8-end c9-start] 1fr [c9-end c10-start] 1fr [c10-end c11-start] 1fr [c11-end c12-start] 1fr [c12-end margin-right-start] 64px [margin-right];
         grid-column-gap: 24px;
         grid-template-rows: auto;
-        border: 2px dotted blue;
+        // border: 2px dotted blue;
 
         & > .description {
+            margin-bottom: 24px;
             grid-column-start: c1-start;
             grid-column-end: span c6-end;
             grid-row-start: 1;
@@ -60,7 +63,7 @@ const HeroContainer = styled.div`
             font-size: 20px;
             line-height: 32px;
             color: ${gray.darker};
-            border: 2px dotted orange;
+            // border: 2px dotted orange;
         }
 
         & > .hero-form {
@@ -73,13 +76,20 @@ const HeroContainer = styled.div`
 
 export default function Hero(props) {
     const { prismicData } = props
-    console.log(red)
+    // console.log(prismicData)
     
     let heroTitle = prismicData.hero_title[0].text
     let heroSubtitle = prismicData.hero_subtitle[0].text
     let heroDescription = prismicData.hero_description[0].text
-    // let heroImageUrl = prismicData.hero_image.url
-    // let heroImageAlt = prismicData.hero_image.alt
+    // let heroButtonText = prismicData.hero_button_text[0].text
+    let formData = {
+        button: {
+            text: prismicData.hero_form_button
+        },
+        inputs: {
+            form: [...prismicData.hero_form]
+        }
+    }
 
     return (
         <HeroContainer>
@@ -87,9 +97,8 @@ export default function Hero(props) {
             <h3 className={'title'}>{heroTitle}</h3>
             <div className={'wrapper'}>
                 <p className={'description'}>{heroDescription}</p>
-                <Form className={'hero-form'} />
+                <Form className={'hero-form'} formData={formData} />
             </div>
-
         </HeroContainer>
     )
 }
