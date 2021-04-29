7210b5980078b7c17c9cb48d4154129f3a42acd7
Marcus Peterson <mdabomb5@gmail.com>
2021-04-29 12:21:52 -0600

    basic file structure for syllabus

---
+++ src/pages/syllabus/development.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout.js"
+import SEO from "../components/seo.js"
+import Syllabus from "../components/syllabus.js"
+
+export default function Development() {
+    return (
+        <Layout>
+            <SEO title={"Development Syllabus"} />
+            <Syllabus image="" convertKitTag="" />
+        </Layout>
+    )
+}
