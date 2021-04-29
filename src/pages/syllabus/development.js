fd4f9b2ed14b3fbe6005f53fa4c34d3b509980aa
Marcus Peterson <mdabomb5@gmail.com>
2021-04-29 13:24:49 -0600

    started the styling

--- src/pages/syllabus/development.js
+++ src/pages/syllabus/development.js
@@ -1,13 +1,13 @@
 import React from "react"
-import Layout from "../components/layout.js"
-import SEO from "../components/seo.js"
-import Syllabus from "../components/syllabus.js"
+import Layout from "../../components/layout.js"
+import SEO from "../../components/seo.js"
+import Syllabus from "../../components/syllabus"
 
 export default function Development() {
     return (
         <Layout>
             <SEO title={"Development Syllabus"} />
-            <Syllabus image="" convertKitTag="" />
+            <Syllabus image="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" convertKitTag="" />
         </Layout>
     )
 }
