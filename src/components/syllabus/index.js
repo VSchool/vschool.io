efad4216a3b423c46c89f6b87b0f4e703381d159
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-29 16:04:43 -0500

    Commit

--- src/components/syllabus/index.js
+++ src/components/syllabus/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroLayout from '../shared/HeroLayout';
 import styled from 'styled-components'
+import { TextInput, Button } from '@vschool/lotus'
 import { red, blue, gray } from '@vschool/lotus'
 
 const Container = styled.section`
@@ -16,9 +17,20 @@ const Container = styled.section`
 const StyledForm = styled.form`
     display: flex;
     flex-direction: column;
+    // border: 2px dashed blue;
 
-    & > div > .first-name {
-        margin-right: 10px;
+    & > .wrapper {
+        display: flex;
+        // border: 1px solid black;
+
+        & > .first-name {
+            margin: 0px;
+            margin-right: 12px;
+        }
+    
+        & > .last-name {
+            margin-left: 12px;
+        }
     }
 
     & > .button-wrapper {
@@ -39,75 +51,8 @@ const StyledForm = styled.form`
     }
 `
 
-const FormInputContainer = styled.div`
-    // border: 2px solid orange;
-    
-    & > .label-container {
-        display: flex;
-        column-gap: 4px;
-        // border: 1px solid orange;
-
-        & > p {
-            // display: none;
-            // display: ${props => props.display};
-            font-family: Aktiv Grotesk Ex;
-            font-style: normal;
-            font-weight: bold;
-            font-size: 12px;
-            line-height: 16px;
-            text-align: right;
-            letter-spacing: 0.5px;
-            color: ${red.base};
-        }
-
-        & > label {
-            margin-bottom: 8px;
-            font-family: Aktiv Grotesk Ex;
-            font-style: normal;
-            font-weight: bold;
-            font-size: 12px;
-            line-height: 16px;
-            display: flex;
-            align-items: center;
-            letter-spacing: 0.5px;
-            color: ${gray.darkest};
-        }
-    }
-
-    & > input {
-        padding: 10px 16px 10px 16px;
-        width: 100%;
-        background: ${gray.lightest};
-        border: 2px solid #514F4B;
-        box-sizing: border-box;
-        font-family: Aktiv Grotesk;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 16px;
-        line-height: 24px;
-        color: ${gray.darker};
-
-        &::placeholder {
-            font-family: Aktiv Grotesk;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 16px;
-            line-height: 24px;
-            color: ${gray.dark};
-        }
-
-        &:empty {
-            border: 2px solid ${gray.base};
-        }
-
-        &:hover {
-            border: 2px solid ${blue.light};
-        }
-
-        &:active {
-            border: 2px solid ${blue.dark};
-        }
-    }
+const StyledInput = styled(TextInput)`
+    margin: 0px;
 `
 
 export default function index(props) {
@@ -116,38 +61,55 @@ export default function index(props) {
             <HeroLayout
                 text={
                     <StyledForm className='hero-form'>
-                        <div style={{display: 'flex'}}>
-                            <FormInputContainer className='first-name'>
-                                <div className='label-container'>
-                                    <label>* First Name</label>
-                                </div>
-                                <input name="firstName" placeholder="John" />
-                            </FormInputContainer>
-
-                            <FormInputContainer className='last-name'>
-                                <div className='label-container'>
-                                    <label>* Last Name</label>
-                                </div>
-                                <input name="lastName" placeholder="Doe" />
-                            </FormInputContainer>
+                        <div className={'wrapper'} >
+                            <StyledInput
+                                type="text"
+                                label="First Name"
+                                name="fName"
+                                placeholder="Your First Name"
+                                value={'fName'}
+                                // onChange={e => setName(e.target.value)}
+                                required
+                                validationText="Required"
+                                className={'first-name'}
+                            />
+                            <StyledInput
+                                type="text"
+                                label="Last Name"
+                                name="lName"
+                                placeholder="Your Last Name"
+                                value={'lName'}
+                                // onChange={e => setName(e.target.value)}
+                                required
+                                validationText="Required"
+                                className={'last-name'}
+                            />
                         </div>
+                            <StyledInput
+                                type="text"
+                                label="Email"
+                                name="email"
+                                placeholder="john@email.com"
+                                value={'email'}
+                                // onChange={e => setName(e.target.value)}
+                                required
+                                validationText="Required"
+                                className={'email'}
+                            />
 
-                        <FormInputContainer className='email'>
-                            <div className='label-container'>
-                                <label>* Email</label>
-                            </div>
-                            <input name="email" placeholder="john@email.com" />
-                        </FormInputContainer>
-
-                        <FormInputContainer className='phone'>
-                            <div className='label-container'>
-                                <label>* Phone</label>
-                            </div>
-                            <input name="phone" placeholder="555-555-5555" />
-                        </FormInputContainer>
-
+                            <StyledInput
+                                type="text"
+                                label="Phone"
+                                name="phone"
+                                placeholder="555-555-5555"
+                                value={'phone'}
+                                // onChange={e => setName(e.target.value)}
+                                required
+                                validationText="Required"
+                                className={'phone'}
+                            />
                         <div className='button-wrapper'>
-                            <button className='form-button'>Download Syllabus</button>
+                            <Button buttonStyle={'primary-dark'} type={'button'} size={'xl'}>{'Download Syllabus'}</Button>
                         </div>
                     </StyledForm>
                 }
@@ -155,8 +117,7 @@ export default function index(props) {
                     <>
                         <img src={props.image} alt=""/>
                     </>
-                }>
-               
+                }>       
             </HeroLayout>
         </Container>
     )
--- src/pages/syllabus/design.js
+++ src/pages/syllabus/design.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../../components/layout.js"
 import SEO from "../../components/seo.js"
 import Syllabus from "../../components/syllabus"
+import designImage from '../../images/syllabus-design-image.png'
+
 
 export default function Design() {
     return (
         <Layout>
             <SEO title={"Design Syllabus"} />
-            <Syllabus image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" convertKitTag="" />
+            <Syllabus image={designImage} convertKitTag="" />
         </Layout>
     )
 }
