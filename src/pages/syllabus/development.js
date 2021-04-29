8dfef7a000dfcfe1f39d018ccc1f84a6c728085f
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-29 16:04:43 -0500

    Commit

--- src/pages/syllabus/development.js
+++ src/pages/syllabus/development.js
@@ -2,12 +2,13 @@ import React from "react"
 import Layout from "../../components/layout.js"
 import SEO from "../../components/seo.js"
 import Syllabus from "../../components/syllabus"
+import developmentImage from '../../images/syllabus-development-image.png'
 
 export default function Development() {
     return (
         <Layout>
             <SEO title={"Development Syllabus"} />
-            <Syllabus image="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" convertKitTag="" />
+            <Syllabus image={developmentImage} convertKitTag="" />
         </Layout>
     )
 }
