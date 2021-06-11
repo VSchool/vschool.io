c7201f2cddb015778ca3c3163c449d9da0af7baa
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-11 02:25:47 -0500

    Updated Schedule.js
    Re-built schedule slider component and is now fully functional. Just needs styling for mobile.

--- src/components/courses/Schedule.js
+++ src/components/courses/Schedule.js
@@ -1,25 +1,139 @@
-import React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { gray } from "@vschool/lotus"
-import tempCalendarImg from '../../images/slider-schedule-mobile-1.svg'
+import indicatorActive from '../../images/slider-indicator-active.svg'
+import indicatorInactive from '../../images/slider-indicator-inactive.svg'
+import previousInactiveMobile from '../../images/slider-previous-inactive-mobile.svg'
+import nextInactiveMobile from '../../images/slider-next-inactive-mobile.svg'
+
+import tempCalendarImg1 from '../../images/slider-schedule-mobile-1.svg'
+import tempCalendarImg2 from '../../images/slider-schedule-mobile-2.svg'
+import tempCalendarImg3 from '../../images/slider-schedule-mobile-3.svg'
+import tempCalendarImg4 from '../../images/slider-schedule-mobile-4.svg'
+import tempCalendarImg5 from '../../images/slider-schedule-mobile-5.svg'
+import tempCalendarImg6 from '../../images/slider-schedule-mobile-6.svg'
+
+const tempArr = [
+    {
+        title: 'Learning That Fits With Your Schedule',
+        body: 'Students will work directly with their student success mentor to set realistic goals based on their life schedule and the time that they have available to work on curriculum.',
+        image: tempCalendarImg1
+    },
+    {
+        title: 'Full Time Schedule And Commitment',
+        body: 'This is an example schedule that demonstrates what a student’s schedule could look like if they have 40 hours available each week and structure their schedule as if they were at their new full-time job.',
+        image: tempCalendarImg2
+    },
+    {
+        title: 'Part Time Job And Commitment',
+        body: 'This is an example schedule that demonstrates what a student’s schedule could look like if they have 20 hours available each week and structure their schedule and are working a part-time job.',
+        image: tempCalendarImg3
+    },
+    {
+        title: 'Full-Time Job - Weekend Warrior',
+        body: 'This is an example schedule that demonstrates what a student’s schedule could look like if they have a commitment to work a full-time job and only have time available for the course on weekends.',
+        image: tempCalendarImg4
+    },
+    {
+        title: 'Life Restrictions - The Night Owl',
+        body: 'This is an example schedule that demonstrates what a student’s schedule could look like if they have a commitment to watch kids or something during the day and only have the night availalbe to work.',
+        image: tempCalendarImg5
+    },
+    {
+        title: 'Our Model Works With Any Schedule',
+        body: 'Work with your student success mentor to configure your schedule to your life situation.',
+        image: tempCalendarImg6
+    }
+]
 
 const StyledSection = styled.section`
+    padding-bottom: 72px;
+    position: relative;
     background-color: ${gray.lighter};
     border:2px solid lightcoral;
+
+    @media (min-width: 800px) {
+        padding: 0px 136px 0px 136px;
+    }
 `
-const Title = styled.h3`
+const Title = styled.h4`
+    margin-bottom: 18px;     
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    font-style: normal;
+    font-weight: 800;
     border: 1px dashed dodgerblue;
 `
 
 const Body = styled.p`
+    margin-bottom: 48px;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 18px;
+    text-align: center;
+    height: 92px;
     border: 1px dashed dodgerblue;
 `
 
-const Image = styled.div`
+const Image = styled.img`
+    margin-bottom: 88px;
     width: 100%;
-    height: 212px;
+    filter: drop-shadow(0px 0.818966px 1.63793px rgba(0, 0, 0, 0.14)) drop-shadow(0px 1.22845px 1.63793px rgba(0, 0, 0, 0.12)) drop-shadow(0px 0.409483px 2.04741px rgba(0, 0, 0, 0.2));
+    // border: 1px dashed dodgerblue;
+`
+
+const SubText = styled.p`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 16px;
+    text-align: center;
     border: 1px dashed dodgerblue;
+`
+
+const IndicatorWrapper = styled.div`
+    position: absolute;
+    top: 408px;
+    display: flex;
+    gap: 16px;
+    height: 20px;
+    // border: 1px solid green;
+`
+
+const SliderIndicator = styled.img`
+    width: 20px;
+    height: 20px;
+    // border: 1px dashed dodgerblue;
+`
+
+const PreviousButton = styled.img`
+    position: absolute;
+    top: 260px;
+    left: 8px;
+    width: 48px;
+    height: 48px;
+    filter: drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.14)) drop-shadow(0px 3px 14px rgba(0, 0, 0, 0.12)) drop-shadow(0px 4px 5px rgba(0, 0, 0, 0.2));
+    // border: 1px dashed blue;
+
+    @media (min-width: 800px) {
+        left: 112px;
+    }
+`
+
+const NextButton = styled.img`
+    position: absolute;
+    top: 260px;
+    right: 8px;
+    width: 48px;
+    height: 48px;
+    filter: drop-shadow(0px 8px 10px rgba(0, 0, 0, 0.14)) drop-shadow(0px 3px 14px rgba(0, 0, 0, 0.12)) drop-shadow(0px 4px 5px rgba(0, 0, 0, 0.2));
+    // border: 1px dashed dodgerblue;
+
+    @media (min-width: 800px) {
+        right: 112px;
+    }
 `
 
 const ComponentName = () => {
@@ -62,20 +176,56 @@ const ComponentName = () => {
         schedule_title: { text: schedTitle },
     } = data.prismicCoursePageSharedData.data
 
-    const tempObj = {
-        title: 'Title',
-        body: 'Body',
-        image: 'Image',
-        caption: 'Captions'
+    const [displayNumber, setDisplayNumber] = useState(0)
+
+    let displayObj = tempArr[displayNumber]
+    const {
+        title,
+        body,
+        image
+    } = displayObj
+
+    function handleNextClick(display, setter) {
+        if (display <= tempArr.length - 1) {
+            setter(() => display + 1)
+        } if (display === undefined) {
+            console.log(`It is about to be undefined`)
+        }
     }
 
-    console.log(555, tempCalendarImg)
+    function handlePreviousClick(display, setter) {
+        if (display > 0) {
+            setter(() => display - 1)
+        }
+    }
 
     return (
         <StyledSection>
-            <Title>{tempObj.title}</Title>
-            <Body>{tempObj.body}</Body>
-            <Image />
+            <Title>{title}</Title>
+            <Body>{body}</Body>
+            <Image src={image} alt={'some text'} />
+            <SubText>{'** Each students schedule will vary depending on their availability and time commitment.'}</SubText>
+            <IndicatorWrapper>
+                {
+                    tempArr.map((obj, idx) => {
+                        if (idx === displayNumber) {
+                            return <SliderIndicator key={idx} src={indicatorActive} alt={'active icon'} />
+                        } else {
+                            return <SliderIndicator key={idx} src={indicatorInactive} alt={'inactive icon'} />
+                        }
+                    })
+                }
+            </IndicatorWrapper>
+            <PreviousButton 
+                src={previousInactiveMobile} 
+                alt={' previous image'} 
+                onClick={() => handlePreviousClick(displayNumber, setDisplayNumber)}
+            />
+            <NextButton 
+                src={nextInactiveMobile} 
+                alt={'next image'} 
+                onClick={() => handleNextClick(displayNumber, setDisplayNumber)}
+            />
         </StyledSection>
     )
 }
