144a96dd4dc96e4a7d694d91d159b9111a8892e5
Bob Ziroll <elderziroll@gmail.com>
2021-06-17 15:05:58 -0600

    Registering new schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,7 @@ module.exports = {
                     "my-base-scholarship": require("./src/schemas/my-base-scholarship.json"),
                     navigation_bar: require("./src/schemas/navigation_bar.json"),
                     payment_options: require("./src/schemas/payment_options.json"),
+                    precourse_communities_page: require("./src/schemas/precourse_communities_page.json"),
                     press: require("./src/schemas/press.json"),
                     rankings_component: require("./src/schemas/rankings_component.json"),
                     responsive_learning: require("./src/schemas/responsive_learning.json"),
