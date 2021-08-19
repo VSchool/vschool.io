a8678cb451cb2861c3b138efd608cbfcb60e94cd
Marcus Peterson <mdabomb5@gmail.com>
2021-08-19 10:52:21 -0600

    added links to download page

--- src/pages/pre-course-partners/download.js
+++ src/pages/pre-course-partners/download.js
@@ -1,13 +1,13 @@
 import React from "react"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
-import Form from "../../components/partners-precourse/pre-course-packet/DownloadXD"
+import Downloader from "../../components/partners-precourse/pre-course-packet/DownloadXD"
 
 export default function PreCourseDownload() {
     return (
         <Layout>
             <Seo title={"Pre Course Partners Packet Download"} />
-            <Form />
+            <Downloader />
         </Layout>
     )
 }
