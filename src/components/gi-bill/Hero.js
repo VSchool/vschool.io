9acdedf1828eee30fbde68ab83a9b9acae75f0bb
Marcus Peterson <mdabomb5@gmail.com>
2022-02-24 15:06:50 -0700

    amending gi-bill page

--- src/components/gi-bill/Hero.js
+++ src/components/gi-bill/Hero.js
@@ -22,40 +22,42 @@ const ContentContainer = styled.div`
 const HeroTitle = styled.h1`
     font-weight: 900;
     color: ${gray.darkest};
-    margin-bottom: 24px;
     font-size: 44px;
     line-height: 48px;
 
     @media (min-width: 800px){
         line-height: 56px;
         font-size: 56px;
-        margin-bottom: 16px;
     }
 `
 
 const HeroDescription = styled.p`
-    margin-bottom: 24px;
+    margin: 12px 0 24px 0;
     font-size: 16px;
     line-height: 24px;
-    font-weight: normal;
+    font-weight: 300;
     color: ${gray.darker};
+    max-width: 445px;
 
     @media (min-width: 800px){
         font-size: 20px;
         line-height: 32px;
-        margin-bottom: 16px;
+        margin: 24px 0 48px 0;
     }
 `
 
-const HeroSubtitle = styled.p`
+
+const BlueSubtext = styled.p`
+    font-family: "aktiv-grotesk-extended";
     font-weight: 800;
-    letter-spacing: 0.25px;
-    text-transform: uppercase;
-    color: ${red.dark};
     font-size: 14px;
     line-height: 20px;
+    letter-spacing: 0.25px;
+    text-transform: uppercase;
+    color: ${blue.base};
+    padding: 8px 0;
 
-    @media (min-width: 800px){
+    @media (min-width: 800px) {
         font-size: 16px;
         line-height: 24px;
     }
@@ -103,7 +105,8 @@ const data = useStaticQuery(graphql`
     hero_title: { text: heroTitle},
     hero_subtitle: { text: heroSubtitle},
     hero_description: { text: heroDescription},
-    hero_form,
+    // hero_gi_bill,
+    // hero_green_checkmark: { url: greenUrl },
     hero_form_button: { text: buttonText},
     hero_form_icon: {
       alt: iconAlt,
@@ -111,7 +114,7 @@ const data = useStaticQuery(graphql`
     },
     hero_image: { url: heroImageUrl, alt: heroImageAlt},
   } = {...data.prismicVeteransLandingPage.data}
-
+  console.log(data.prismicVeteransLandingPage.data)
     return (
         <HeroContainer>
             <ContentContainer>
@@ -120,13 +123,18 @@ const data = useStaticQuery(graphql`
                         text={
                             <>
                                 <div>
-                                    <HeroSubtitle>{heroSubtitle}</HeroSubtitle>
+                                    <BlueSubtext>{heroSubtitle}</BlueSubtext>
                                     <HeroTitle>{heroTitle}</HeroTitle>
                                 </div>
                                 <HeroDescription>
-                                    {heroDescription}
+                                  Use your G.I. Bill® to learn to <b>code</b> or <b>design</b> and land a job in tech. Talk with us today to learn more.
                                 </HeroDescription>
-                                <Form formInfo={hero_form} buttonText={buttonText} type='hero' icon={{iconAlt, iconUrl}} />
+                                {/* <Form formInfo={hero_form} buttonText={buttonText} type='hero' icon={{iconAlt, iconUrl}} /> */}
+                                <Form icon={{iconAlt, iconUrl}} buttonText={buttonText} />
+                                <div>
+                                  {/* <img src={greenUrl} /> */}
+                                  {/* <p>{heroGi}</p> */}
+                                </div>
                             </>
                         }
                         image={
