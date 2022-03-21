2fd8407332c81fde126889f21a29751aa27bc48b
Marcus Peterson <mdabomb5@gmail.com>
2022-03-21 13:16:17 -0600

    added GTM code

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,6 +61,13 @@ export default function IndexPage({ data }) {
 
             {/* Insert Pinterest claim code into head of index page */}
             <Helmet>
+                {/* Google Tag Manager */}
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5MHHFVP');</script>
+                {/* End Google Tag Manager  */}
                 <meta
                     name="p:domain_verify"
                     content="e7ec9d78e5ee9bcdffca6388ba0cdfa8"
