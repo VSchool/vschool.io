3c449d13261a0632baf8e2710023d440096a3b19
Marcus Peterson <mdabomb5@gmail.com>
2023-04-17 13:39:21 -0600

    added linkedin pixel

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import Link from "../shared/QueryLink"
 import SubscribeForm from "./SubscribeForm.js"
 import FooterSubInformation from "./FooterSubInformation.js"
+import { Helmet } from "react-helmet"
 
 const Container = styled.footer`
     background-color: ${black};
@@ -294,6 +295,29 @@ const Footer = () => {
             <GiBill>{giBill}<GiLink to={giLink}> {giLink}</GiLink></GiBill>
             </FooterContainer>
             <FooterSubInformation />
+            <Helmet>
+                <script type="text/javascript">
+                    {`_linkedin_partner_id = "4855186";
+                    window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                    window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
+                </script>
+                <script type="text/javascript">
+                    {`(function(l) {
+                        if (!l){
+                            window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                            window.lintrk.q=[]
+                        }
+                        var s = document.getElementsByTagName("script")[0];
+                        var b = document.createElement("script");
+                        b.type = "text/javascript";b.async = true;
+                        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                        s.parentNode.insertBefore(b, s);
+                    })(window.lintrk);`}
+                </script>
+                <noscript>
+                    <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=4855186&fmt=gif" />
+                </noscript>
+            </Helmet>
         </Container>
     )
 }
