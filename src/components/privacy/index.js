f19a182290387b9c71ef932d8c2b7fdda1093bdb
Marcus Peterson <mdabomb5@gmail.com>
2023-06-26 12:53:49 -0600

    privacy policy page

---
+++ src/components/privacy/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "./Header.js"
+import Table from "./Table.js"
+import Policy from "./Policy.js"
+
+export default function CareersPage() {
+    return (
+        <>
+            <Header />
+            <Table />
+            <Policy />
+        </>
+    )
+}
